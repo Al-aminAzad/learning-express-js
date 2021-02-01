58d8d5d06f0bd3ee1d93b6018b77a47baa5cd04e
Al-aminAzad <alaminazad240@gmail.com>
2021-02-01 17:19:03 +0600

    I have learned all abbout expres() functions

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+const { Router } = require('express');
 const express = require('express');
 const app = express();
 // app.use(express.json());
 // app.use(express.raw());
-app.use(express.urlencoded());
-app.get('/', (req, res) => {
+// app.use(express.urlencoded());
+// app.use(
+//   express.static(`${__dirname}/public/`, {
+//     index: 'home.html',
+//   })
+// );
+
+const router = express.Router({
+  caseSensitive: true,
+});
+app.use(router);
+router.get('/', (req, res) => {
   res.send('This is home page');
 });
 app.post('/', (req, res) => {
   console.log(req.body);
   res.send('This is home page with post request');
 });
-app.get('/about', (req, res) => {
+router.get('/about', (req, res) => {
   res.send('<h1>this is about page</h1>');
 });
 app.get('*', (req, res) => {
