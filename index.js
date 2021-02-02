a7669e33e4bfb9ab2a9dfa41ea7d185a4956888e
Al-aminAzad <alaminazad240@gmail.com>
2021-02-02 20:04:29 +0600

    all methods and properties of response

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 const express = require('express');
-const cookieParse = require('cookie-parser');
-const handle = require('./handle')
 const app = express();
-app.use(express.json());
-app.use(cookieParse());
-const adminRoute = express.Router();
-adminRoute.get('/dashboard', (req, res) => {
-  // console.log(req.originalUrl);
-  // console.log(req.url);
-  // console.log(req.path);
-  // console.log(req.hostname);
-  console.log(req.ip);
-  res.send('We are in admin dashboard');
+
+app.set('view engine', 'ejs');
+app.get('/test', (req, res) => {
+  res.send('Redirect from about');
 });
-app.use('/admin', adminRoute);
-app.get('/user/:id', handle);
-app.post('/user', (req, res) => {
-  console.log(req.route);
-  res.send('Hello world post');
+
+app.get('/about', (req, res) => {
+  // console.log(res.headersSent);
+  // res.render('pages/about', {
+  //   name: 'Bangladesh',
+  // });
+  // console.log(res.headersSent);
+  // res.send('About page');
+  // res.end();
+  // res.json({
+  //   name: 'Bangladesh',
+  //   language: 'Bangla',
+  // });
+  // res.status(200);
+  // res.end();
+  // res.sendStatus(500);
+  // res.format({
+  //   'text/plain': () => {
+  //     res.send('Hi');
+  //   },
+  //   'text/html': () => {
+  //     res.render('pages/about', {
+  //       name: 'Bangladesh',
+  //     });
+  //   },
+  //   'application/json': () => {
+  //     res.json({
+  //       message: 'About',
+  //     });
+  //   },
+  //   default: ()=>{
+  //     res.status(403).send('Not acceptable');
+  //   }
+  // });
+  // res.cookie('name','Alamin azad',{
+
+  // });
+  // res.end();
+  // res.redirect('/test');
+  // res.end();
+  res.set('platform', 'learn with sumit');
+  console.log(res.get('platform'));
+  res.end();
 });
+
 app.listen(3000, () => {
-  console.log('listening port on 3000');
+  console.log('listening in port 3000');
 });
---
+++ request.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cookieParse = require('cookie-parser');
+const handle = require('./handle');
+const app = express();
+app.use(express.json());
+app.use(cookieParse());
+const adminRoute = express.Router();
+adminRoute.get('/dashboard', (req, res) => {
+  // console.log(req.originalUrl);
+  // console.log(req.url);
+  // console.log(req.path);
+  // console.log(req.hostname);
+  console.log(req.ip);
+  res.send('We are in admin dashboard');
+});
+app.use('/admin', adminRoute);
+app.get('/user/:id', handle);
+app.post('/user', (req, res) => {
+  console.log(req.route);
+  res.send('Hello world post');
+});
+app.listen(3000, () => {
+  console.log('listening port on 3000');
+});
